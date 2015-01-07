854680c29039d8378fa2bf2a9fd3a9d5df7b604b
Arthur <arthur_wang@adsame.com>
2015-01-07 15:27:43 +0800

    add gp.py

---
+++ utility/gp.py
@@ -0,0 +1,31 @@
+#--coding:utf8--
+import sys
+import gevent
+from gevent import monkey
+
+monkey.patch_all()
+
+shgps = ['sh601179', 'sh600219', 'sz000725']
+url_head = 'http://hq.sinajs.cn/list='
+
+if sys.version_info[0] == 3:
+    from urllib.request import urlopen
+else:
+    from urllib2 import urlopen
+
+def print_current_price(id):
+    url = url_head + id
+    content = urlopen(url).read()
+    data = content.split(",")
+    name = data[0].split('"')[1]
+    name = name.decode('gbk')
+    price = data[3]
+    t = data[31]
+    
+    print('id: %s, %s, c: %r, t: %s' % (id, name, price, t))
+
+while(1):
+    jobs = [gevent.spawn(print_current_price, id) for id in shgps]
+    gevent.wait(jobs)
+    gevent.sleep(5)
+    print '############'
